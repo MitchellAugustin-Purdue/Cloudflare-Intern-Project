0f498e2dd2595b3d4d963486237e551b6be02699
Mitchell Augustin <mitchell@mitchellaugustin.com>
2021-11-13 11:55:34 -0500

    First real commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { Router } from "@reach/router";
+
+import Posts from './components/posts'
+import Post from './components/post'
+
+function App() {
+  return (
+    <html>Hello World</html>
+  );
+}
+
+export default App;
